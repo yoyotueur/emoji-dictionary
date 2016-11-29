04f25e4ee086bcdbd18e4219ee321bcc8dfef2f6
Johan Bertin <johan.bertin@gmail.com>
2016-11-29 18:51:05 +0100

    Segue 1

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -32,6 +32,15 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
         return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "🎙")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
